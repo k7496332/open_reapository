52f36e77cd23c83fcac8ab1acc88ac8a0105a71d
k7496332 <130883171+k7496332@users.noreply.github.com>
2024-10-29 14:38:26 +0900

    Create create_figure_in_cm.py

---
+++ program/tool/create_figure_in_cm.py
@@ -0,0 +1,6 @@
+def create_figure_in_cm(width_cm,height_cm):
+    width_inch = width_cm / 2.54
+    height_inch = height_cm / 2.54
+
+    fig = plt.figure(figsize=(width_inch, height_inch))
+    return fig
